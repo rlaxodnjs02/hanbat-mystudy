6d76e9891381e521f10fb65aad9172690f83a081
rlaxodnjs02 <phjtwth17@gmail.com>
2025-07-31 15:01:45 +0900

    SpringBoot로 웹 만들기

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,25 +1,21 @@
 package org.example.myapp;
 
-import com.google.common.base.Joiner;
-import org.example.util.Utils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다. <br>
- * <code>App</code> 클래스!
+/*
+// 스프링 부트를 실행할 수 있도록 기본 정보를 자동 설정
+@EnableAutoConfiguration
+
+// 현재 클래스와 같은 패키지 및 하위 패키지를 다 뒤져서 컴포넌트를 찾아 자동으로 생성하라는 설정
+// @Component, @Controller, @Service, @Repository 등이 붙은 클래스를 찾음
+@ComponentScan
  */
+
+@SpringBootApplication // = @SpringBootConfigration + @EnableAutoConfigration + @ComponentScan
 public class App {
-  /**
-   * 애플리케이션의 진입점(entry point) 역할을 하는 메서드입니다. 프로그램을 실행하면 "hello, world"를 콘솔에 출력합니다.
-   *
-   * @param args 프로그램 실행 시 전달되는 아규먼트 배열
-   * @author 한밭대학교
-   * @version 1.0
-   * @since 2025-07-29
-   * @see <a href="https://docs.oracle.com/en/java/javase/21/">JDK Documentation</a>
-   */
   public static void main(String[] args) {
-    Utils.printMessage("hello, world");
-    String message = Joiner.on(", ").join("hello", "world");
-    System.out.println(message);
+    SpringApplication.run(App.class, args);
+    System.out.println("스프링부트 서버 시작!");
   }
 }
